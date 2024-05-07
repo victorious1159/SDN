17efa6f53e48d7826c7aa27ba48269c4266f1a9e
Victorious1159 <xzonetran@gmail.com>
2024-05-07 14:29:16 +0700

    exe2

--- index.js
+++ index.js
@@ -3,14 +3,16 @@
     	function solveRect(l,b) {
     	    console.log("Solving for rectangle with l = " + l + " and b = " + b);
     	
-    	    if (l <= 0 || b <= 0) {
-    	        console.log("Rectangle dimensions should be greater than zero:  l = "
-	               + l + ",  and b = " + b);
-    	    }
-    	    else {
-    	        console.log("The area of the rectangle is " + rect.area(l,b));
-    	        console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    	    }
+            rect(l,b, (err,rectangle) => {
+                if (err) {
+                    console.log("ERROR: ", err.message);
+                }
+                else {
+                    console.log("The area of the rectangle is " + rectangle.area());
+                    console.log("The perimeter of the rectangle is " + rectangle.perimeter());
+                }
+            });
+            console.log("This statement after the call to react()");
     	}
     	
     	solveRect(2,4);
